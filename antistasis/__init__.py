38f89686dcc86bb30d5e89275cedbd45f311f8b3
nicmower <nicmower@gmail.com>
2024-12-20 11:55:05 -0500

    centralized version in py file

--- antistasis/__init__.py
+++ antistasis/__init__.py
@@ -2,7 +2,4 @@
 
 # Import the main game launcher
 from .main import run_game
-
-# Provide metadata (optional)
-__version__ = "0.0.1"
-__author__ = "Nic Mower"
+from .version import __version__, __title__, __author__
---
+++ antistasis/__version__.py
@@ -0,0 +1,4 @@
+# version.py
+__version__ = "0.0.1"
+__title__ = "Antistasis"
+__author__ = "Nic Mower"
